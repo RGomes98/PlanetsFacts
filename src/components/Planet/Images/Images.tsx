b61efe60c5912ac8bc81306865e9517ca029f13d
Renan Gomes <renango2015@gmail.com>
2024-01-12 17:05:45 -0300

    fix: move static assets to public folder

--- src/components/Planet/Images/Images.tsx
+++ src/components/Planet/Images/Images.tsx
@@ -4,14 +4,13 @@ export const Images = ({ name, images }: { name: string; images: string[] }) => 
   return (
     <div className={styles.container}>
       {images.map((image, index) => {
-        const imgUrl = new URL(image, import.meta.url).href;
         const isGeologySelected = index === 1;
 
         return (
           <img
             alt={name}
             key={image}
-            src={imgUrl}
+            src={image}
             className={`${styles.image} ${styles[name.toLowerCase()]} ${
               (isGeologySelected && styles.geologyImage) || ''
             }`}
